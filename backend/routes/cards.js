5fa8e5bb0a12a56b3e2bc1f0abeca46ce14ada59
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-06-02 01:00:50 +0300

    Feat: add error handling

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
-const { getAllCards, createCard, deleteCard, likeCard, dislikeCard } = require("../controllers/cards");
+const {
+  getAllCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require("../controllers/cards");
 
 router.get("/cards", getAllCards);
 router.post("/cards", createCard);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
-const { getAllUsers, getUserId, createUser, updateProfile, updateAvatar } = require("../controllers/users");
+const {
+  getAllUsers,
+  getUserId,
+  createUser,
+  updateProfile,
+  updateAvatar,
+} = require("../controllers/users");
 
 router.get("/users", getAllUsers);
 router.get("/users/:userId", getUserId);

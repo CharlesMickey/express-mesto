e4d8f8d57fe1ddc4e4a414b7cf4410d8c44fe178
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-06-01 15:22:37 +0300

    Feat: add models: user and card

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});

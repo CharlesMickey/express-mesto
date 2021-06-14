d9131c616e6f9af8ac53a449df0f917fffbeb9d5
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-06-15 00:00:15 +0300

    Feat: do not return the password hash

--- controllers/users.js
+++ controllers/users.js
@@ -77,6 +77,7 @@ module.exports.login = (req, res) => {
   const { email, password } = req.body;
 
   User.findOne({ email })
+    .select("+password")
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error("Неправильные почта или пароль"));
--- models/user.js
+++ models/user.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 

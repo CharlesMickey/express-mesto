3b0ea37114acf98733259c2e9a090815ac97fd9d
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-06-20 16:36:47 +0300

    Feat: add crash test

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 app.post(
   "/signin",
   celebrate({
